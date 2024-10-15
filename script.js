f8759c0b321be1b9a1d55f3a6cf1067eecbfbf16
Sifatsagar <160644486+Sifatsagar@users.noreply.github.com>
2024-10-14 22:47:02 -0400

    hope sp it works

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const backToHomeButton = document.querySelector(".back-home-button");
+
+    backToHomeButton.addEventListener("click", function(event) {
+        event.preventDefault();  // Prevent the default anchor behavior
+        window.location.href = "index.html";  // Redirect to the homepage
+    });
+});
